72aa237737d93c7698564b2fec509a8473db3def
Felix Mairamhof <feberg.k@gmail.com>
2024-05-24 08:32:53 +0200

    Build working delete Function

--- React/weather-app/src/components/Navbar.jsx
+++ React/weather-app/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar(props) {
                                 <div className="flex items-center">
                                     <Link to={`/weather/${position.city}`} className="inline-block border border-blue-500 rounded py-2 px-6 m-2 bg-blue-500 hover:bg-blue-700 text-white">
                                         {position.city}
-                                        <button onClick={() => props.onDelete(index)} className="ml-2 border border-red-500 rounded px-2 py-1 bg-red-500 hover:bg-red-700 text-white">Delete</button>
+                                        <button onClick={() => props.onDelete(index)} className="px-2 ml-2 rounded-2xl bg-blue-600 hover:bg-blue-800 text-white">X</button>
 
                                     </Link>
                                 </div>
